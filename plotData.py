98ad125a55a1d6002a9fc0768463d4d13899c231
trond <trond@300e1062-d174-49dd-b3a7-dc8cb9d2c987>
2009-01-08 16:37:15 +0000

    General commit that includes working horizontal and vertical interpolation routines
    git-svn-id: https://subversion.assembla.com/svn/soda2roms@17 300e1062-d174-49dd-b3a7-dc8cb9d2c987

--- plotData.py
+++ plotData.py
@@ -4,18 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap, NetCDFFile
 
-def contourMap(grd,data,depthlevel,var):
+def contourMap(grdROMS,grdSODA,data,depthlevel,var):
     
  
-    tlat      = np.array(grd.lat_rho)
-    tlon      = np.array(grd.lon_rho)
+    tlat      = np.array(grdROMS.lat_rho)
+    tlon      = np.array(grdROMS.lon_rho)
     temp      = data
     
     
     # make longitudes monotonically increasing.
     tlon = np.where(np.greater_equal(tlon,min(tlon[:,0])),tlon-360,tlon)
     
-    plt.figure(figsize=(6,6))
+    plt.figure(figsize=(12,12))
 
     # Plot the Georges Bank region
     #map = Basemap(llcrnrlon=-80.5,llcrnrlat=32.5,urcrnrlon=-58,urcrnrlat=45.5,
@@ -24,11 +24,13 @@ def contourMap(grd,data,depthlevel,var):
     # Plot the Nordic region
     map = Basemap(llcrnrlon=-80,llcrnrlat=31.5,urcrnrlon=-55,urcrnrlat=45,
             resolution='h',projection='tmerc',lon_0=-65,lat_0=40)
-    
+   
     map.drawcoastlines()
     map.fillcontinents(color='white')
     
     x, y = map(tlon,tlat)
+    
+    x2,y2=map(np.array(grdSODA.lon),np.array(grdSODA.lat))
    # im = map.pcolor(x,y,temp,\
    #            shading='faceted',cmap=plt.cm.cool,vmin=0,vmax=0)
     # disclaimer:  these are not really the grid cells because of the
@@ -42,9 +44,12 @@ def contourMap(grd,data,depthlevel,var):
     map.fillcontinents(color='white')
     
     CS1 = map.contourf(x,y,temp,15)
+    plt.colorbar(orientation='horizontal')
     #CS2 = map.contour(x,y,temp,15,colors='black',linewidths=0.5)
     plt.title('Variabel %s at depth %s'%(var,depthlevel))
     
-    #plt.show()
-    plotfile='figures/soda_depthK_'+str(depthlevel)+'.png'
-    plt.savefig(plotfile)
+    map.plot(x2,y2,'bo')
+     
+    plt.show()
+    #plotfile='figures/soda_depthK_'+str(depthlevel)+'.png'
+    #plt.savefig(plotfile)
