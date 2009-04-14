b1ab488481d95b739c435f5f02c6e531079f76c7
trond <trond@300e1062-d174-49dd-b3a7-dc8cb9d2c987>
2009-04-14 21:24:31 +0000

    Minor changes
    git-svn-id: https://subversion.assembla.com/svn/soda2roms@53 300e1062-d174-49dd-b3a7-dc8cb9d2c987

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import soda2roms, IOstation
 import clim2bry
 import grd
+import numpy as np
 
 __author__   = 'Trond Kristiansen'
 __email__    = 'trond.kristiansen@imr.no'
@@ -14,44 +15,60 @@ __status__   = "Development"
 
 def help():
     """
-    This program is run by typing: python main;py in the command window.
+    This program is run by typing: python main.py in the command window.
     """
     
 def main():
     print '\n--------------------------\n'
-    print '\n---> Initializing\n'
     print 'Started ' + time.ctime(time.time())
     
     climName='test.nc'
     initName='test_init.nc'
     bryName='test_bry.nc'
     
+    write_clim=True
+    write_bry=False
+    write_init=True
+    write_stations=False
+    
     sodapath="/Volumes/HankRaid/SODA/"
     romsgridpath="/Users/trond/Projects/arcwarm/nordic/AA_10km_grid.nc"
     romsgridpath="/Users/trond/ROMS/GoM/grid/gom_grd.nc"
   #  romsgridpath="/Users/trond/Projects/arcwarm/nordic/imr_nordic_4km.nc"
-    start_year=1960
-    end_year=1961
-    start_day_in_start_year=10
-    end_day_in_end_year=65
+    start_year      =1960
+    end_year        =1961
+    start_julianday =1
+    end_julianday   =365
+    
+    print '\n'
+    print 'Conversions runs from day %s in %s to day %s in year %s'%(start_julianday,start_year,end_julianday,end_year)
+    print 'Output grid file is: %s'%(romsgridpath)
+    print '\nThe following functions are run:'
+    if write_clim is True: print '--->write clim (%s)'%(climName)
+    if write_init is True: print '--->write init (%s)'%(initName)
+    if write_bry is True: print '--->write bry (%s)'%(bryName)
+    if write_stations is True: print '-->-write stations'
+    print '\n--------------------------\n'
+    print '\n---> Initializing\n'
+    
+    start_day_in_start_year=np.round(start_julianday/5.0)
+    end_day_in_end_year=round(end_julianday/5.0)
     
     years=[(int(start_year)+kk) for kk in range(int(end_year)-int(start_year))]
-    
-    IDS=[(i+1) for i in range(73)]
+
+    IDS=[(i+1+int(start_day_in_start_year)) for i in range(int(end_day_in_end_year))]
     
     soda2roms.convertSODA2ROMS(years,IDS,climName,initName,sodapath,romsgridpath)
     
-    
-    grdROMS = grd.grdClass(romsgridpath,"ROMS")
-    
-    #clim2bry.writeBry(grdROMS,'1960',bryName,climName)   
+    if write_bry is True:
+        grdROMS = grd.grdClass(romsgridpath,"ROMS")
+        clim2bry.writeBry(grdROMS,'1960',bryName,climName)   
 
-    # GB, NovaScotia, Grand Bank, Nuuk, Iceland, NS, Lofoten, BS
-   
-        
-    lonlist=[-66.5]#,-66.40,-50.43,-54.38, 21.51,  1.53, 13.38, 32.75]
-    latlist=[ 41.5]#, 43.41, 44.50, 64.72, 63.35, 58.36, 67.50, 71.77]
-    #IOstation.getStationData(years,IDS,outfilename,sodapath,latlist,lonlist)
+    if write_stations is True:
+        # GB, NovaScotia, Grand Bank, Nuuk, Iceland, NS, Lofoten, BS     
+        lonlist=[-66.5]#,-66.40,-50.43,-54.38, 21.51,  1.53, 13.38, 32.75]
+        latlist=[ 41.5]#, 43.41, 44.50, 64.72, 63.35, 58.36, 67.50, 71.77]
+        IOstation.getStationData(years,IDS,outfilename,sodapath,latlist,lonlist)
 
     print 'Finished ' + time.ctime(time.time())
     
