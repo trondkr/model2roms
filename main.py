468956e5666e1961d8f333890be9c0f2373b6f7b
trond <trond@300e1062-d174-49dd-b3a7-dc8cb9d2c987>
2009-04-15 18:31:33 +0000

    Cleaned up the main script so that most of the editable lines such as paths and boolean operators are located in the main.py file
    git-svn-id: https://subversion.assembla.com/svn/soda2roms@57 300e1062-d174-49dd-b3a7-dc8cb9d2c987

--- main.py
+++ main.py
@@ -31,6 +31,11 @@ def main():
     write_init=True
     write_stations=False
     
+    """Set show_progress to "False" if you do not want to see the progress
+    indicator for horizontal interpolation. This requires the two modules:
+    terminal.py and progressBar.py"""
+    show_progress=True
+    
     sodapath="/Volumes/HankRaid/SODA/"
     romsgridpath="/Users/trond/Projects/arcwarm/nordic/AA_10km_grid.nc"
     romsgridpath="/Users/trond/ROMS/GoM/grid/gom_grd.nc"
@@ -38,18 +43,22 @@ def main():
     start_year      =1960
     end_year        =1961
     start_julianday =1
-    end_julianday   =365
+    end_julianday   =50
     
-    print '\n'
-    print 'Conversions runs from day %s in %s to day %s in year %s'%(start_julianday,start_year,end_julianday,end_year)
-    print 'Output grid file is: %s'%(romsgridpath)
+    vars=['temperature','salinity','ssh','uvel','vvel']
+    vars=['temperature']
+    print 'Conversions run from day %s in %s to day %s in year %s'%(start_julianday,start_year,end_julianday,end_year)
+    print 'The following variables will be converted:'
+    for var in vars:
+        print '---> %s'%(var)
+    print '\nOutput grid file is: %s'%(romsgridpath)
     print '\nThe following functions are run:'
-    if write_clim is True: print '--->write clim (%s)'%(climName)
-    if write_init is True: print '--->write init (%s)'%(initName)
-    if write_bry is True: print '--->write bry (%s)'%(bryName)
-    if write_stations is True: print '-->-write stations'
+    if write_clim is True: print '---> write clim (%s)'%(climName)
+    if write_init is True: print '---> write init (%s)'%(initName)
+    if write_bry is True: print '---> write bry (%s)'%(bryName)
+    if write_stations is True: print '---> write stations'
     print '\n--------------------------\n'
-    print '\n---> Initializing\n'
+    print '\n---> Initializing'
     
     start_day_in_start_year=np.round(start_julianday/5.0)
     end_day_in_end_year=round(end_julianday/5.0)
@@ -58,7 +67,7 @@ def main():
 
     IDS=[(i+1+int(start_day_in_start_year)) for i in range(int(end_day_in_end_year))]
     
-    soda2roms.convertSODA2ROMS(years,IDS,climName,initName,sodapath,romsgridpath)
+    soda2roms.convertSODA2ROMS(years,IDS,climName,initName,sodapath,romsgridpath,vars,show_progress)
     
     if write_bry is True:
         grdROMS = grd.grdClass(romsgridpath,"ROMS")
