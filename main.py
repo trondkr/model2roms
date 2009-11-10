c78fc15341e22e3d43ad07968504e0d8ee962c54
trond <trond@300e1062-d174-49dd-b3a7-dc8cb9d2c987>
2009-11-10 14:47:37 +0000

    Many bug fixes and changes after switching to 64 bit system and also enabling more generic use of monthly SODA data
    git-svn-id: https://subversion.assembla.com/svn/soda2roms@83 300e1062-d174-49dd-b3a7-dc8cb9d2c987

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import soda2roms, IOstation
 import clim2bry, DecimateGrid
 import grd
 import numpy as np
+import ncarRiver
+#import ncarSSS
+
 
 __author__   = 'Trond Kristiansen'
 __email__    = 'trond.kristiansen@imr.no'
 __created__  = datetime(2009, 1,30)
-__modified__ = datetime(2009,10, 1)
-__version__  = "1.2"
+__modified__ = datetime(2009,11, 9)
+__version__  = "1.3"
 __status__   = "Development"
 
 def help():
@@ -32,45 +35,61 @@ def main():
     print '\n--------------------------\n'
     print 'Started ' + time.ctime(time.time())
     
-    climName='gom_clim_1990.nc'
-    initName='gom_init_1990.nc'
-    bryName='gom_bry_1990.nc'
+    """ EDIT ==================================================================="""
     
     """Set show_progress to "False" if you do not want to see the progress
     indicator for horizontal interpolation. This requires the two modules:
     terminal.py and progressBar.py"""
     show_progress=True
-    write_stations=False
+    extractStations=False
     createForcing=True
-    decimateGrid=False 
+    decimateGrid=False
+    createRiverRunoff=False
     
+    """Define the paths to the CORE data (only river file), and the SODA/HYCOM data"""
+    corepath="/Users/trond/Projects/arcwarm/CORE/"
     sodapath="/Volumes/HankRaid/SODA/"
     sodapath="/Volumes/MacintoshHD2/Datasets/SODA/"
     sodapath="/Volumes/MacintoshHD2/Datasets/SODAMonthly/"
+    sodapath="/Users/trond/Projects/arcwarm/SODAmonthly/"
     hycompath="/Users/trond/Projects/arcwarm/SODA/HYCOM/"
     
-    #romsgridpath="/Users/trond/Projects/arcwarm/nordic/AA_10km_grid.nc"
     romsgridpath="/Volumes/HankRaid/ROMS/GoM/grid/gom_grd.nc"
-    #romsgridpath="/Users/trond/ROMS/GoM/grid/gom_grd_030208.nc"
+    romsgridpath="/Users/trond/Projects/Roms/GOMfull/Inputs/gom_grd.nc"
     #romsgridpath="/Users/trond/Projects/arcwarm/nordic/imr_nordic_4km.nc"
     #romsgridpath="/Users/trond/Projects/arcwarm/SODA/soda2roms/imr_nordic_8km.nc"
-    #romsgridpath="/Users/trond/Projects/Nathan/NoMed47_GRID_Global.nc"
-    #romsgridpath='/Users/trond/Projects/Nathan/GOM_GRID_Global.nc'
-    start_year      =1959
-    end_year        =1964
+    
+    start_year      =1960
+    end_year        =1961
     start_julianday =0
     end_julianday   =365
+    
+    """Name of output files for CLIM, BRY, and INIT files"""
+    climName='gom_clim_'+str(start_year)+'.nc'
+    initName='gom_init_'+str(start_year)+'.nc'
+    bryName='gom_bry_'+str(start_year)+'.nc'
     
     """Set the input data MODEL type: Current options are SODA or HYCOM"""
     type='HYCOM' 
     type='SODA'
     type='SODAMONTHLY'
     
+    """Define what variables to include in the forcing files"""
     vars=['temperature','salinity','ssh','uvel','vvel']
-    #vars=['temperature']
+    #vars=['uvel','vvel']
     
+    """5 day or 30 day average files for SODA"""
     if type=='SODA': aveDays=5.0
     if type=='SODAMONTHLY': aveDays=30.0
+    
+    """Define a set of longitude/latitude positions with names to extract into
+    station files (using extractStations)"""
+    stationNames=['NorthSea','Iceland','EastandWestGreenland','Lofoten', 'Georges Bank']
+    lonlist=[ 2.4301, -22.6001, -47.0801,  13.3801, -67.2001]
+    latlist=[54.5601, 63.7010,  60.4201,  67.5001,  41.6423]
+    
+        
+    """" NO EDIT BELOW ========================================================="""
     
     start_day_in_start_year=np.round(start_julianday/aveDays)
     end_day_in_end_year=round(end_julianday/aveDays)
@@ -95,21 +114,18 @@ def main():
         elif type=='HYCOM':
             soda2roms.convertMODEL2ROMS([1],[1,2,3],climName,initName,hycompath,romsgridpath,vars,show_progress,type)
     
-        clim2bry.writeBry(grdROMS,'1960',bryName,climName)
+        clim2bry.writeBry(grdROMS,start_year,bryName,climName)
     
+    if createRiverRunoff is True:
+        ncarRiver.runoff(grdROMS,corepath)
     
     
     if decimateGrid==True:
         DecimateGrid.createGrid(grdROMS,'/Users/trond/Projects/arcwarm/SODA/soda2roms/imr_nordic_8km.nc',2)
      
 
-    if write_stations is True:
-        print "Running in station mode"
-        stationNames=['NorthSea','Iceland','EastandWestGreenland','Lofoten', 'Georges Bank']
-        lonlist=[ 2.4301, -22.6001, -47.0801,  13.3801, -67.2001]
-        latlist=[54.5601, 63.7010,  60.4201,  67.5001,  41.6423]
-    
-    
+    if extractStations is True:
+        print "Running in station mode and extracting pre-defined station locations"
         IOstation.getStationData(years,IDS,sodapath,latlist,lonlist,stationNames)
 
     print 'Finished ' + time.ctime(time.time())
