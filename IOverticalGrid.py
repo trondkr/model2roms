7fdbfc759d6c07bef68bcb635809e437b46b826d
trond <trond@300e1062-d174-49dd-b3a7-dc8cb9d2c987>
2009-11-11 20:52:24 +0000

    Added possibility of generating vstretching=2 type of s-levels. Set vstretching=2 in grd.py to use these functions instead of the default Haidvogel stretching
    git-svn-id: https://subversion.assembla.com/svn/soda2roms@92 300e1062-d174-49dd-b3a7-dc8cb9d2c987

--- IOverticalGrid.py
+++ IOverticalGrid.py
@@ -7,8 +7,8 @@ __author__   = 'Trond Kristiansen'
 __email__    = 'trond.kristiansen@imr.no'
 __created__  = datetime(2008, 8, 15)
 __modified__ = datetime(2008, 8, 19)
-__modified__ = datetime(2009, 10, 1)
-__version__  = "1.2"
+__modified__ = datetime(2009, 11, 11)
+__version__  = "1.3"
 __status__   = "Development"
 
 
@@ -32,7 +32,7 @@ def calculate_z_w(self):
     (zeta varies), and also position dependent since the bottom matrix varies.
     Results are stored in array z[eta_rho, xi_rho, s]
      
-    Trond Kristiansen, 20.01.2008, 03.12.2008, 09.12.2008
+    Trond Kristiansen, 20.01.2008, 03.12.2008, 09.12.2008, 11.11.2009
     """
     sc_w=np.zeros((self.Nlevels+1),np.float64)
     Cs_w=np.zeros((self.Nlevels+1),np.float64)
@@ -45,36 +45,61 @@ def calculate_z_w(self):
     for k in xrange(self.Nlevels+1):
         sc_w[k]=-1.0+(self.Nlevels-k)*cff
     
-        Ptheta=np.sinh(self.theta_s*sc_w[k])/np.sinh(self.theta_s)
-        Rtheta=np.tanh(self.theta_s*(sc_w[k]+0.5))/(2.0*np.tanh(0.5*self.theta_s))-0.5
+        if self.vstretching==1:
+            """
+            Original vertical strectching function, Song and Haidvogel (1994).
+            see: https://www.myroms.org/wiki/index.php/Vertical_S-coordinate
+            C(s) = (1 - b) * [SINH(s * a) / SINH(a)] +
     
-        if self.theta_s != 0.0:
-            Cs_w[k]=(1-self.theta_b)*Ptheta+self.theta_b*Rtheta
-        else:
-            Cs_w[k]=sc_w[k]
+             b * [-0.5 + 0.5 * TANH(a * (s + 0.5)) / TANH(0.5 * a)]
+            """
+            Ptheta=np.sinh(self.theta_s*sc_w[k])/np.sinh(self.theta_s)
+            Rtheta=np.tanh(self.theta_s*(sc_w[k]+0.5))/(2.0*np.tanh(0.5*self.theta_s))-0.5
             
+            if self.theta_s != 0.0:
+                Cs_w[k]=(1-self.theta_b)*Ptheta+self.theta_b*Rtheta
+            else:
+                Cs_w[k]=sc_w[k]
+                
+        if self.vstretching==2:
+            """
+            A. Shchepetkin new vertical stretching function.
+            C(s) = [1.0 - COSH(theta_s * s)] / [COSH(theta_s) - 1.0]
+            Adapted from set_scoord.F (ROMS source code)
+            """
+            Aweight=1.0
+            Bweight=1.0
+          
+            if self.theta_s > 0.0:
+                Csur=(1.0 - np.cosh(self.theta_s * sc_w[k]))/(np.cosh(self.theta_s) - 1.0)
+                    
+                if self.theta_b > 0.0:
+              
+                    Cbot=np.sinh(self.theta_b)*(sc_w[k] + 1.0)/np.sinh(self.theta_b) - 1.0
+    
+                    Cweight=(sc_w[k]+1.0)**Aweight*(1.0 + (Aweight/Bweight)*(1.0-(sc_w[k]+1.0)**Bweight))
+               
+                    Cs_w[k]=Cweight*Csur +(1.0-Cweight)*Cbot
+                else:
+                    Cs_w[k]=Csur
+            else:
+                Cs_w[k]=sc_w
+                
    
-    """
-    TODO: FIXME hardcode variables should be read from file
-    """
     zeta=None
-    
+     
     for k in xrange(len(sc_w)):
-        scmCshc=(np.subtract(sc_w[k],Cs_w[k]))*hc
-        z_w[k,:,:] = scmCshc + np.multiply(Cs_w[k],h)
-        
-        if zeta != None:
-            dd = np.divide(zeta,h)
-            z_w[k,:,:] = z_w[k,:,:] + scmCshc*dd
+        if self.vstretching==1:
+            z_w[k,:,:] =np.multiply(sc_w[k],hc) + np.subtract(h,hc)*Cs_w[k]
             
+        if self.vstretching==2:
+            z_w[k,:,:] =np.multiply(sc_w[k],hc) + np.subtract(h,hc)*Cs_w[k]
+   
     self.z_w = z_w
     self.Cs_w=Cs_w
     self.hc=hc
     self.s_w=sc_w
     
-    #for l in xrange(10,400,20):
-    #    for ll in xrange(10,300,20):
-    #        print z_w[:,ll,l],self.depth[ll,l]
   
 def calculate_z_r(self):
     
@@ -95,31 +120,67 @@ def calculate_z_r(self):
     cff=1.0/float(self.Nlevels)
             
     for k in xrange(self.Nlevels):
+        """
+        Original vertical strectching function, Song and Haidvogel (1994).
+        see: https://www.myroms.org/wiki/index.php/Vertical_S-coordinate
+        C(s) = (1 - b) * [SINH(s * a) / SINH(a)] +
+
+             b * [-0.5 + 0.5 * TANH(a * (s + 0.5)) / TANH(0.5 * a)]
+        """
         sc_r[k]=-1.0+(self.Nlevels-k-0.5)*cff
     
-        Ptheta=np.sinh(self.theta_s*sc_r[k])/np.sinh(self.theta_s)
-        Rtheta=np.tanh(self.theta_s*(sc_r[k]+0.5))/(2.0*np.tanh(0.5*self.theta_s))-0.5
+        if self.vstretching==1:
+            Ptheta=np.sinh(self.theta_s*sc_r[k])/np.sinh(self.theta_s)
+            Rtheta=np.tanh(self.theta_s*(sc_r[k]+0.5))/(2.0*np.tanh(0.5*self.theta_s))-0.5
+        
+            if self.theta_s != 0.0:
+                Cs_r[k]=(1-self.theta_b)*Ptheta+self.theta_b*Rtheta
+            else:
+                Cs_r[k]=sc_r[k]
+                
+        if self.vstretching==2:
+            """
+            A. Shchepetkin new vertical stretching function.
+            C(s) = [1.0 - COSH(theta_s * s)] / [COSH(theta_s) - 1.0]
+            Adapted from set_scoord.F (ROMS source code)
+            """
+            Aweight=1.0
+            Bweight=1.0
+          
+            if self.theta_s > 0.0:
+                Csur=(1.0 - np.cosh(self.theta_s * sc_r[k]))/(np.cosh(self.theta_s) - 1.0)
+                    
+                if self.theta_b > 0.0:
+              
+                    Cbot=np.sinh(self.theta_b)*(sc_r[k] + 1.0)/np.sinh(self.theta_b) - 1.0
     
-        if self.theta_s != 0.0:
-            Cs_r[k]=(1-self.theta_b)*Ptheta+self.theta_b*Rtheta
-        else:
-            Cs_r[k]=sc_r[k]
-
+                    Cweight=((sc_r[k]+1.0)**Aweight)*(1.0 + (Aweight/Bweight)*(1.0-(sc_r[k]+1.0)**Bweight))
+               
+                    Cs_r[k]=Cweight*Csur +(1.0-Cweight)*Cbot
+                else:
+                    Cs_r[k]=Csur
+            else:
+                Cs_r[k]=sc_r
+       
     """
     TODO: FIXME hardcode variables should be read from file
     """
     zeta=None
-    
+     
     for k in xrange(len(sc_r)):
-        scmCshc=(np.subtract(sc_r[k],Cs_r[k]))*hc
-        z_r[k,:,:] = scmCshc + np.multiply(Cs_r[k],h)
-        
-        if zeta != None:
-            dd = np.divide(zeta,h)
-            z_r[k,:,:] = z_r[k,:,:] + scmCshc*dd
-  
+        if self.vstretching==1:
+            z_r[k,:,:] =np.multiply(sc_r[k],hc) + np.subtract(h,hc)*Cs_r[k]
+            
+        if self.vstretching==2:
+            z_r[k,:,:] =np.multiply(sc_r[k],hc) + np.subtract(h,hc)*Cs_r[k]
+   
     self.z_r = z_r
     self.Cs_rho=Cs_r
     self.hc=hc
     self.s_rho=sc_r
+    
+    #print 'max',self.z_r.max(), 'stretching function=',self.vstretching
+    #print 'min',self.z_r.min()
+    #import sys
+    #sys.exit()
     
